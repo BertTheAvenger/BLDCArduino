f056fce34952f0a13e16d8349a31109eaf203ebb
BertTheAvenger <45153623+BertTheAvenger@users.noreply.github.com>
2018-12-21 01:43:26 -0800

    Add files via upload

--- BLDC11/BLDC11.ino
+++ BLDC11/BLDC11.ino
@@ -1,5 +1,5 @@
 #include <Encoder.h>
-#define psclr true
+#define psclr false
 
 #define motor false
 
@@ -20,6 +20,8 @@ int wavePos[3] = {0, (arrLength/3), (2*arrLength/3)}; //Init motor positions in 
 int waveForm[arrLength];
 int modForm[arrLength];
 
+int phasePwm[3] = {0,0,0}; //Keeps track of phase PWMs.
+
 double angle[arrLength];
 Encoder enc(2,3);
 
@@ -36,6 +38,9 @@ void setup() {
   }
   Serial.begin(9600);
   pinMode(13, OUTPUT);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
 }
 
 
@@ -45,57 +50,73 @@ int dir = 1;
 int pos = 0;
 int stage = 0;
 
-byte mode = 0; //Start in off.
-
+byte mode = 0;
 boolean recievingPacket = false;
 int packetLength = 0;
 int packetIndex = 0;
-int inBuffer[256]; //define as maximum packet length.
+volatile int inBuffer[256]; //define as maximum packet length.
 void loop() {
-  if(Serial.available() > 0)
-  {
+  //PACKET HANDLING BELOW
+  if(Serial.available() > 0){
     byte b = Serial.read(); //Unsigned byte
     //Serial.write(5);
     
-    if(!recievingPacket && b == 0) //Not recieving packet & new packet command recieved
-    {
+    if(!recievingPacket && b == 0){ //Not recieving packet & new packet command recieved
         recievingPacket = true;
         packetIndex = 0;
         packetLength = 0;
     }
-    else if(recievingPacket && packetIndex == 0) //Catch second byte, first recorded byte, which is message command.
-    {
+    else if(recievingPacket && packetIndex == 0){ //Catch second byte, first recorded byte, which is message command.
         packetLength = getCommandLength(b); //Write packet length
         inBuffer[packetIndex] = b;
         packetIndex++;
-        if(packetLength == 1) //If 1 byte command, finish after first command byte.
-        {
+        if(packetLength == 1){ //If 1 byte command, finish after first command byte.
           finishPacket();
         }
     }
-    else if(recievingPacket && packetIndex < packetLength - 1) //If recieving and in range, just keep adding the bytes.
-    {
+    else if(recievingPacket && packetIndex < packetLength - 1){ //If recieving and in range, just keep adding the bytes.
         inBuffer[packetIndex] = b;
         packetIndex++;
     }
-    else if(recievingPacket)//Last expected byte in, Packet is finished, analyze.
-    {
+    else if(recievingPacket){//Last expected byte in, Packet is finished, analyze.
         inBuffer[packetIndex] = b;
         finishPacket();
     }
     //Otherwise, not part of packet. Just let it go.
   }
+  
+  
+}
 
-  switch(mode)
+void setMode(byte newMode)
+{
+  switch(newMode)
   {
-    case 0:
-    break;
-    case 1:
-    break;
-    case 2:
-    break;
-    case 3:
-    break;
+    case 0: //Motor stopped mode
+      analogWrite(A, 0);
+      analogWrite(B, 0);
+      analogWrite(C, 0);
+      mode = 0;
+      break;
+    case 1: //Remote Phase Control Mode
+      mode = 1;
+      break;
+  }
+}
+
+void setPhase(byte phase, byte pwm)
+{
+  
+  if(mode == 1) //Make sure in correct mode.
+  {
+    
+    if(phase == 1){
+      analogWrite(A, (int)pwm);
+    } else if(phase == 2){
+      analogWrite(B, (int)pwm);
+    }else if(phase == 3){
+      analogWrite(C, (int)pwm);
+    }
   }
   
 }
@@ -109,32 +130,47 @@ void finishPacket()
 
 void parseCommands()
 {
+
   byte cmdBuffer[256];
-  switch(inBuffer[0]) //Switch based on command byte.
-  {
-    case 1: //ACK
-      cmdBuffer[0] = 1;
-      sendByteArray(cmdBuffer, 1);
-      break;
-    case 3://Add shorts
-      long a = 0;
-      long b = 0;
-      a |= inBuffer[1] << 8; //Upper part of int.
-      a |= inBuffer[2]; //Lower part of int.
-      b |= inBuffer[3] << 8;
-      b |= inBuffer[4];
-      long ret = a+b;
-      cmdBuffer[0] = 3;
-      cmdBuffer[1] = (uint8_t)(ret >> 24); //Shift down top byte, cast to byte.
-      cmdBuffer[2] = (uint8_t)(ret >> 16);
-      cmdBuffer[3] = (uint8_t)(ret >> 8);
-      cmdBuffer[4] = (uint8_t)ret;
-      sendByteArray(cmdBuffer, 5);
-      break;
-    case 4: //setMode
-      mode = inBuffer[1];     
-  }
+  //if(inBuffer[0] == 4){digitalWrite(13, HIGH);}
+  byte b = inBuffer[0];
   
+  if(b == 1){ //Switch statement wasn't working, so if-else ladder it is.
+    sendAck();
+    
+  } else if(b == 2){
+    setPhase(inBuffer[1], inBuffer[2]);
+    sendAck();
+    
+  } else if(b == 3){
+    long a = 0;
+    long b = 0;
+    a |= inBuffer[1] << 8; //Upper part of int.
+    a |= inBuffer[2]; //Lower part of int.
+    b |= inBuffer[3] << 8;
+    b |= inBuffer[4];
+    long ret = a+b;
+    cmdBuffer[0] = 3;
+    cmdBuffer[1] = (uint8_t)(ret >> 24); //Shift down top byte, cast to byte.
+    cmdBuffer[2] = (uint8_t)(ret >> 16);
+    cmdBuffer[3] = (uint8_t)(ret >> 8);
+    cmdBuffer[4] = (uint8_t)ret;
+    sendByteArray(cmdBuffer, 5);
+    
+  } else if(b == 4){
+    setMode(inBuffer[1]);
+    sendAck();
+    
+  } else if(b == 5){
+    
+  }
+
+}
+
+void sendAck()
+{
+  byte outBuffer[] = {1};
+  sendByteArray(outBuffer, 1);
 }
 
 void sendByteArray(byte toSend[], int commandLength)
@@ -148,41 +184,16 @@ void sendByteArray(byte toSend[], int commandLength)
   Serial.write(outBuffer, commandLength + 1);
 }
 
-int getCommandLength(uint8_t command)
+int getCommandLength(byte command)
 {
   switch(command)
   {
-    case 1 : return 1;
-    case 3 : return 5;
+    case 1 : return 1; //ACK
+    case 2 : return 3; //SetPhase
+    case 3 : return 5; //Addshorts
+    case 4 : return 2; //Setmode
+    case 5 : return 1; //GetTotalVals
   }
-}
-
-int increment(int dir)
-{
-  wavePos[0] = wrapIntRealitive(wavePos[0] + dir, 0, arrLength-1);
-  wavePos[1] = wrapInt(wavePos[1] + dir, 0, arrLength-1);
-  wavePos[2] = wrapInt(wavePos[2] + dir, 0, arrLength-1);
-  return wavePos[0];
-}
-
-int wrapIntRealitive(int x, int low, int high)
-{
-  
-  if(x<low){stage = wrapInt(stage -1, 0, cyclesPerRot - 1); return high;}
-  else if(x>high){stage = wrapInt(stage + 1, 0, cyclesPerRot - 1); return low;}
-  else{return x;}
-}
-
-int wrapInt(int x, int low, int high)
-{
-  if(x<low){return high;}
-  else if(x>high){return low;}
-  else{return x;}
-}
-
-int sign(double x)
-{
-  return abs(x)/x;
 }
 
 double readAmps() //Averages out I readings over 100ms
@@ -190,13 +201,11 @@ double readAmps() //Averages out I readings over 100ms
   const int readingsNumber = 20;
   const int duration = 20;
   long sum = 0;
-  
   for(int i = 0; i < readingsNumber; i++)
   {
     sum += analogRead(ISense);
     delay(duration/readingsNumber);
   }
-  
   int avg = (int)(sum/((long)readingsNumber));
   double amps = ((double)(avg - 512) / 512.0) * 5.0;
   return amps;
@@ -204,8 +213,8 @@ double readAmps() //Averages out I readings over 100ms
 
 double readVolts()
 {
-  const int readingsNumber = 20;
-  const int duration = 20;
+  const int readingsNumber = 20; //Amount of readings
+  const int duration = 20; //Time to gather readings.
   long sum = 0;
   
   for(int i = 0; i < readingsNumber; i++)
